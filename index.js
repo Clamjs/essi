e23416103f04cd8660eac5126965925ff13ce689
limingv5 <limingv5@gmail.com>
2015-05-05 15:22:57 +0800

    0.0.9 pre

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ var ESSI = require("./api");
 var trace = require("plug-trace");
 
 var pkg = require(__dirname + "/package.json");
-require("check-update")({
-  packageName: pkg.name,
-  packageVersion: pkg.version,
-  isCLI: process.title == "node"
-}, function (err, latestVersion, defaultMessage) {
-  if (!err && pkg.version < latestVersion) {
-    console.log(defaultMessage);
-  }
-});
+var starter = process.argv[1];
+if (!new RegExp("clam$").test(starter)) {
+  require("check-update")({
+    packageName: pkg.name,
+    packageVersion: pkg.version,
+    isCLI: new RegExp(pkg.name + '$').test(starter)
+  }, function (err, latestVersion, defaultMessage) {
+    if (!err && pkg.version < latestVersion) {
+      console.log(defaultMessage);
+    }
+  });
+}
 
 function init_config(dir) {
   var pathLib = require("path");
